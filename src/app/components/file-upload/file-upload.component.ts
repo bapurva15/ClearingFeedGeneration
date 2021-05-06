d5bd3cfc96953c1e02af30963a33c22f86a4c691
arundhati-c <81864366+arundhati-c@users.noreply.github.com>
2021-05-06 10:53:22 +0530

    File Upload added
    file upload (and read and validate) working.

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from 'src/app/services/file-upload.service';
 
 @Component({
   selector: 'app-file-upload',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent implements OnInit {
+ // Variable to store shortLink from api response
+ shortLink: string = "";
+ loading: boolean = false; // Flag variable
+ file?: File; // Variable to store file
 
-  constructor() { }
+ // Inject service 
+ constructor(private service: FileUploadService) { }
 
-  ngOnInit(): void {
-  }
+ ngOnInit(): void {
+ }
 
+ // On file Select
+ onChange(event:any) {
+     this.file = event.target.files[0];
+ }
+
+ // OnClick of button Upload
+ onUpload() {
+     this.loading = !this.loading;
+     console.log(this.file);
+     this.service.uploadFile(this.file).subscribe(
+         (event) => {
+             if (typeof (event) === 'object') {
+
+                 // Short link via api response
+                 this.shortLink = event.link;
+
+                 this.loading = false; // Flag variable 
+             }
+         }
+     );
+ }
 }
